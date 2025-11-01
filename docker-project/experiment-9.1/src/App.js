cf336a4d76dda8c472729581610eee8f5d06d3e5
Mohit kumar <23bai70230@cuchd.in>
2025-11-01 21:42:37 +0530

    added to experiment-9

---
+++ docker-project/experiment-9.1/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>🚀 Successfully Dockerized! 🚀</h1>
+        <p>
+          This React application is now running live from an Nginx server
+          inside a Docker container.
+        </p>
+        <p>
+          This is the new content.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
